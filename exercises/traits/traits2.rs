1290b8bb419aa157fbcde27b8b8735ccc9e6de54
Justis <justis710@gmail.com>
2021-05-03 12:51:14 -0400

    traits + results1 done

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -10,13 +10,19 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
-
 trait AppendBar {
-    fn append_bar(self) -> Self;
+    fn append_bar(&mut self) -> Self;
 }
 
 //TODO: Add your code here
+
+impl AppendBar for Vec<String> {
+	fn append_bar(&mut self) -> Self {
+		let bar = String::from("Bar");
+		self.push(bar);
+		self.to_vec()
+	}
+}
 
 #[cfg(test)]
 mod tests {
@@ -25,6 +31,7 @@ mod tests {
     #[test]
     fn is_vec_pop_eq_bar() {
         let mut foo = vec![String::from("Foo")].append_bar();
+        assert_eq!(foo, vec![String::from("Foo"), String::from("Bar")]);
         assert_eq!(foo.pop().unwrap(), String::from("Bar"));
         assert_eq!(foo.pop().unwrap(), String::from("Foo"));
     }
